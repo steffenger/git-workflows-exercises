3e7f0c7480b36353f7e81352074323d6c575a6ac
ac135212 <steffen@isd-vision.isd.uni-stuttgart.de>
2023-04-25 10:15:06 +0200

    solve task B vector substraction

--- exercise1.py
+++ exercise1.py
@@ -45,4 +45,8 @@ def test_3d_vector_addition() -> None:
 
 def test_3d_vector_subtraction() -> None:
     # Task B: add a test for vector subtraction
-    assert False
+    v = Vector([1.0, 2.0, 3.0]) - Vector([0.9, 1.0, 1.5])
+    assert isclose(v[0], 0.1)
+    assert isclose(v[1], 1.0)
+    assert isclose(v[2], 1.5)
+    
